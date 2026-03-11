bbaf9f27245f93590f662c444f46abe128af3489
Samuel Proulx <samuel@interfree.ca>
2026-03-11 17:46:42 -0400

    Add config for packaging.

---
+++ dist/config.lua
@@ -0,0 +1,8 @@
+mpackage = [[sral-mudlet]]
+author = [[fastfinge]]
+title = [[direct screen reader output]]
+description = [[
+Screen reader output for mudlet, using screen reader API's. 
+]]
+version = [[1.0.0]]
+created = [[2026-03-11]]
